9abb9de42d744a776143baa4cb0c443b16c2ff09
Karen Mondoro <karen.mondoro@comcast.net>
2018-05-18 09:52:05 -0400

    working through RoomList errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
@@ -19,13 +19,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <RoomList />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Bloc Chat</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          Pic a topic and chat away!
+        </p> */}
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 
+
 class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       rooms: []
     };
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+    
   }
 
   render() {
     return (
-      <section className='rooms'>
-      
-      </section>
-
+      <div>
+        RoomList Component
+      </div>
     );
   }
-
 }
 
 
